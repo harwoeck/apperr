52c9ba3f3a5fb2131fcae1aac3c2dd2090f818e2
Florian Harwoeck <florian@harwoeck.com>
2021-07-21 17:36:11 +0200

    Open Source i18n adapter from internal version 918c7e1
    Signed-off-by: Florian Harwoeck <florian@harwoeck.com>

---
+++ adapter/i18n/go.mod
@@ -0,0 +1,9 @@
+module github.com/harwoeck/apperr/adapter/i18n
+
+go 1.16
+
+require (
+	github.com/harwoeck/apperr/apperr v1.0.0
+	github.com/nicksnyder/go-i18n/v2 v2.1.2
+	golang.org/x/text v0.3.6
+)
---
+++ adapter/i18n/i18n.go
@@ -0,0 +1,43 @@
+package i18n
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/harwoeck/apperr/apperr"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+type adapter struct {
+	bundle *i18n.Bundle
+}
+
+func (a *adapter) Localize(msgID string, languages []string) (msg string, tag language.Tag, notFound bool, err error) {
+	msg, tag, err = i18n.NewLocalizer(a.bundle, languages...).LocalizeWithTag(&i18n.LocalizeConfig{
+		MessageID: msgID,
+	})
+	if err != nil && strings.Contains(err.Error(), "not found") {
+		return "", language.Und, true, nil
+	}
+	return
+}
+
+func (a *adapter) LocalizeFromConfig(cfg interface{}, languages []string) (msg string, tag language.Tag, notFound bool, err error) {
+	locCfg, ok := cfg.(*i18n.LocalizeConfig)
+	if !ok {
+		return "", language.Und, false, fmt.Errorf("apperr/adapter/i18n.LocalizeFromConfig: unable to type assert interface cfg to *i18n.LocalizeConfig")
+	}
+
+	msg, tag, err = i18n.NewLocalizer(a.bundle, languages...).LocalizeWithTag(locCfg)
+	if err != nil && strings.Contains(err.Error(), "not found") {
+		return "", language.Und, true, nil
+	}
+	return
+}
+
+func NewI18nAdapter(bundle *i18n.Bundle) apperr.LocalizationProvider {
+	return &adapter{
+		bundle: bundle,
+	}
+}
